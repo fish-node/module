c8940f5381cff5594e87b2cfac7ac81eacc38ecf
bitfishxyz <bitfishxyz@gmail.com>
2019-06-05 11:34:54 +0800

    init

--- commonjs/README.md
+++ commonjs/README.md
@@ -11,4 +11,4 @@
 - require其他模块，就是寻找对应模块的module.exports这个对象
 - exports是前者的引用，修改exports的属性就是修改module.exports,但是修改exports的引用，那它就不能代表molude.exports,但是其他文件的require只看module.exports
 
-说了这么多，我只想说：设计exports的人是大傻逼。 为了所谓的简写，为什么把这么简单的事件搞得复杂？能不能统一一下？浪费了多少脑细胞？？
+说了这么多，我只想说：设计exports的人是大**。 为了所谓的简写，为什么把这么简单的事件搞得复杂？能不能统一一下？浪费了多少脑细胞？？
--- commonjs/index.js
+++ commonjs/index.js
@@ -2,6 +2,20 @@ let a = require('./a')
 let b = require('./b')
 let c = require('./c')
 
+/**
+ * 说明在module.exports后的代码也会被执行
+ */
 console.log(a) // { name: 'aaaaa' }
+
+/**
+ * exports是module.exports的引用
+ * 真正require()的是module.exports
+ * 修改exports的引用后，不会到处exports引用的对象
+ */
 console.log(b) // {}
+
+/**
+ * 在exports指向module.exports时,
+ * 可以通过修改exports的值来修改要导出的对象
+ */
 console.log(c) // { name: 'cccc' }
